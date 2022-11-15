a4467af10ce902583919cc7adce1db9486c2e569
Ali Madooei <alimadooei@gmail.com>
2022-11-15 09:46:44 -0500

    Step 22: MongoDB & Mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bookmarks from "./routes/bookmarks.js";
+import * as db from "./data/db.js";
 
+db.connect();
 const app = express();
 
 app.use(express.json());
