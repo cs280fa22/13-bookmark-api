ee697abeaf7c3e214a07f4cb18760573169c1d53
Ali Madooei <alimadooei@gmail.com>
2022-11-15 08:00:19 -0500

    Step 3: Vitest

--- src/model/Bookmark.js
+++ src/model/Bookmark.js
@@ -1,6 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
 
 class Bookmark {
+  // pre: title is a non-empty string
+  //      and url is a valid url
   constructor(title, url) {
     this.id = uuidv4();
     this.title = title;
